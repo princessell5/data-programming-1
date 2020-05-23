5d448eb30fac17f2b7886df8999617934423bc30
stem league <64894084+stemleague@users.noreply.github.com>
2020-05-23 16:04:38 -0700

    Create space.py

---
+++ space.py
@@ -0,0 +1,22 @@
+# Find the bug in our Spaceship's Program
+
+height = 72
+
+if height > 20:
+  print('stratosphere')
+elif height > 50:
+  print('mesosphere')
+elif height > 100:
+  print('space')
+else:
+  print('troposhere') 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+# Try writing your own Program that utilize if statements! Be creative :)
